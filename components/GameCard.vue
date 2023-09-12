d84cb33b1c57812c536e0f10946ca5bd41550ce2
BANO.notIT <bano.notit@yandex.ru>
2023-09-12 22:53:02 +0300

    feat: Implement GameCard

---
+++ components/GameCard.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import OutlineHeartIcon from "@heroicons/vue/24/outline/HeartIcon"
+import SolidHeartIcon from "@heroicons/vue/24/solid/HeartIcon"
+
+const { game, liked = false, stockAmount } = defineProps<{
+  game: {
+    link: string,
+    image: string,
+    name: string
+  }
+  liked?: boolean,
+  stockAmount?: string
+}>()
+
+const emit = defineEmits<{
+  (event: "update:liked", state: boolean): void
+}>()
+
+</script>
+
+<template>
+  <div class="relative cardContainer" :class="{liked}">
+    <div class="imageWrapper relative w-full block p-[1px]">
+      <span v-if="stockAmount" class="absolute right-4 top-4 px-2 text-white rounded-full bg-white/25">
+        {{ stockAmount }}
+      </span>
+      <img :src="game.image" alt="" role="presentation" class="w-full h-full">
+    </div>
+
+    <div class="buttonsContainer grid items-start justify-between max-w-full mt-4 gap-2 whitespace-normal">
+      <nuxt-link class="text-white font-bold linkContainer break-words" :to="game.link">
+        {{ game.name }}
+      </nuxt-link>
+      <button class="likeButton" @click="emit('update:liked', !liked)">
+        <component :is="liked ? SolidHeartIcon : OutlineHeartIcon " class="h-6 w-6 text-blue-500 hover:text-blue-400" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+.linkContainer::before {
+  content: ' ';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+}
+
+.imageWrapper {
+  aspect-ratio: 190 / 136;
+  background: rgba(255, 255, 255, 0.02);
+  border-radius: 12px;
+}
+
+.imageWrapper::before {
+  content: "";
+  pointer-events: none;
+  user-select: none;
+  position: absolute;
+  padding: 1px;
+  inset: 0;
+  border-radius: 12px;
+  background: linear-gradient(#FFFFFF26, #FFFFFF0A);
+  /*noinspection CssInvalidPropertyValue*/
+  mask-mode: match-source, match-source;
+  mask-repeat: repeat, repeat;
+  mask-clip: content-box, border-box;
+  mask-origin: content-box, border-box;
+  mask-position: 0% 0%, 0% 0%;
+  mask-size: auto, auto;
+  mask-image: linear-gradient(black, black), linear-gradient(black, black);
+  mask-composite: exclude;
+}
+
+.cardContainer:hover .imageWrapper {
+  background: rgba(255, 255, 255, 0.1);
+}
+
+.cardContainer.liked .imageWrapper {
+  background-image: linear-gradient(#3478F6, #5991FF);
+}
+
+.imageWrapper img {
+  object-fit: contain;
+}
+
+.cardContainer.liked:hover .imageWrapper {
+  background: #3478F6;
+}
+
+.likeButton {
+  z-index: 2;
+}
+
+.buttonsContainer {
+  grid-template-columns: auto min-content;
+}
+</style>
