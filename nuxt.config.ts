20519ae3d68826354132f0d5d4075482f0d0f7d8
BANO.notIT <bano.notit@yandex.ru>
2023-09-13 13:54:49 +0300

    feat: Setup i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    target: 'static',
-    router: {
-        base: '/game-values/'
-    },
-    modules: [
-      "tailwindcss"
-    ],
-    css: ["~/assets/css/main.css"],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        },
-    },
-    build: {
-      transpile: ["@heroicons/vue"]
+  target: "static",
+  router: {
+    base: "/game-values/"
+  },
+  modules: [
+    "tailwindcss",
+    "@nuxtjs/i18n"
+  ],
+  i18n: {
+    strategy: "prefix_except_default",
+    locales: ["en", "de"],
+    defaultLocale: "en",
+    vueI18n: "./i18n.config.ts"
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
     }
+  },
+  build: {
+    transpile: ["@heroicons/vue"]
+  }
 })
