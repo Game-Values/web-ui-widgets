83359824627019b5b814c74d4ef6471095810b35
GVTech <tech@game-values.com>
2023-10-09 00:04:36 +0200

    cfg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: '/web-ui-widgets/',
-    // buildAssetsDir: 'assets'
+    baseURL: '/web-ui-widgets/',
+    buildAssetsDir: 'assets'
   },
   // target: "static",
   modules: [
