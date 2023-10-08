bf2407af17480fb944a05c44b503662dffe02cdb
GVTech <tech@game-values.com>
2023-10-08 22:27:37 +0200

    Table Component

--- components/data/Table.vue
+++ components/data/Table.vue
@@ -1,17 +1,26 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue"
+import { HandThumbUpIcon, ArrowSmallUpIcon } from "@heroicons/vue/24/solid/esm"
 
 export default defineComponent({
+  components: {
+    HandThumbUpIcon,
+    ArrowSmallUpIcon
+  },
   props: {
     data: {
       type: Array as PropType<Array<Record<string, any>>>,
       required: true
+    },
+    id: {
+      type: String,
+      required: false
     }
   },
   data() {
     return {
       currentPage: 1,
-      itemsPerPage: 9,
+      itemsPerPage: 9
     }
   },
   computed: {
@@ -40,6 +49,15 @@ export default defineComponent({
       if (this.currentPage > 1) {
         this.currentPage--
       }
+    },
+    truncate(str?: string, length: number = 5): string {
+      if (!str) return ""
+
+      if (str.length <= length) {
+        return str
+      }
+
+      return str.substring(0, length) + "..."
     }
   }
 })
@@ -48,48 +66,79 @@ export default defineComponent({
 <template>
   <div class="min-w-full divide-y divide-gray-200">
     <div class="min-w-full divide-y divide-solarized-base1 bg-solarized-base03 p-6 rounded-lg shadow-lg">
-      <table class="min-w-full w-full table-fixed">
+      <table class="min-w-full w-full table-fixed" :id="id">
         <thead class="text-solarized-base0">
         <tr>
           <!-- Seller Column Title -->
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th class="px-6 py-3 w-48 flex-none text-left text-xs font-medium text-gray-100 uppercase tracking-wider">
             Seller
           </th>
           <!-- Other Columns Titles -->
-          <th v-for="key in headers" v-if="key !== 'Seller'" :key="key"
-              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            {{ key }}
-          </th>
+          <template v-for="header in headers">
+            <th v-if="header !== 'Seller'" :key="header"
+                class="px-6 py-3 flex-grow text-left text-xs font-medium text-gray-100 uppercase tracking-wider">
+              {{ header }}
+            </th>
+          </template>
         </tr>
         </thead>
         <tbody class="text-solarized-base1">
-        <tr v-for="item in paginatedData" :key="JSON.stringify(item)">
+        <tr v-for="item in paginatedData" :key="JSON.stringify(item)"
+            class="hover:bg-solarized-base02 hover:rounded-lg transition-colors duration-300">
           <!-- Seller Column -->
-          <td class="px-6 py-4 whitespace-nowrap">
+          <td class="px-6 py-4 w-48 flex-none whitespace-nowrap">
             <div class="flex">
               <!-- Left Column: Logo and Name -->
               <div class="flex-shrink-0 flex flex-col items-center mr-4">
-                <img :src="'/userlogo.jpg'" class="h-8 w-8 rounded-full" alt="User Image" />
-                <span class="mt-2">{{ item.Seller.name }}</span>
+                <img :src="'/userlogo.jpg'" class="h-11 w-11 rounded-full" alt="User Image" />
+                <!--                <span class="mt-2 text-xs">{{ truncateName(item.Seller.name) }}</span>-->
               </div>
               <!-- Right Column: Deals, Likes, and Rating -->
-              <div class="flex-grow flex flex-col justify-between">
-                <div>{{ item.Seller.deals }} deals</div>
-                <div>{{ item.Seller.liked }} {{ item.Seller.rating }}</div>
+              <div class="flex-grow flex flex-col justify-between space-y-1">
+                <!-- This div displays the deals info -->
+                <div class="text-xs text-gray-100">{{ truncate(item.Seller.name, 16) }}</div>
+                <div class="text-xs text-gray-500">{{ item.Seller.deals }} deals compl.</div>
+                <!-- This flex container arranges its children in a row layout -->
+                <div class="flex text-xs space-x-2">
+                  <!-- HandThumbUpIcon + liked in green -->
+                  <div class="flex items-center text-green-500">
+                    <HandThumbUpIcon class="h-4 w-4 mr-1" />
+                    <span>{{ item.Seller.liked }}</span>
+                  </div>
+                  <!-- ArrowsUpDownIcon + rating in blue -->
+                  <div class="flex items-center text-blue-500">
+                    <ArrowSmallUpIcon class="h-4 w-4 mr-1" />
+                    <span>{{ item.Seller.rating }}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </td>
           <!-- Other Columns -->
-          <td v-for="key in headers" v-if="key !== 'Seller'" :key="key" class="px-6 py-4 whitespace-nowrap">
-            <div v-if="typeof item[key] === 'object'">
-              <div v-for="(value, subKey) in item[key]" :key="subKey">
-                <strong>{{ subKey }}:</strong> {{ value }}
+          <template v-for="header in headers">
+            <td v-if="header !== 'Seller'" :key="header" class="px-6 py-4 flex-grow whitespace-nowrap">
+              <div v-if="typeof item[header] === 'object'">
+                <div v-for="(value, subKey) in item[header]" :key="subKey">
+                  <strong>{{ subKey }}:</strong> {{ value }}
+                </div>
               </div>
-            </div>
-            <div v-else>
-              {{ item[key] }}
-            </div>
-          </td>
+              <div v-else>
+                <div v-if="header === 'Description'" class="text-sm w-full whitespace-pre-line">
+                  <div class="text-gray-200">
+                    {{ item[header] }}
+                  </div>
+                  {{ truncate(item["Text"], 50) }}
+                </div>
+                <div v-else-if="header === 'Price'" class="flex items-center">
+                  <span class="mr-1 text-gray-100">{{ item[header] }}</span>
+                  <span>€</span>
+                </div>
+                <div v-else>
+                  {{ item[header] }}
+                </div>
+              </div>
+            </td>
+          </template>
         </tr>
         </tbody>
       </table>
@@ -113,7 +162,6 @@ export default defineComponent({
         </button>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -132,7 +180,7 @@ export default defineComponent({
   }
 
   .bg-solarized-base03 {
-    background-color: #002b36;
+    background-color: #1B1B1C;
   }
 
   .text-solarized-base1 {
@@ -145,6 +193,10 @@ export default defineComponent({
 
   .divide-solarized-base1 > :not(template) ~ :not(template) {
     border-color: #93a1a1;
+  }
+
+  .bg-solarized-base02 {
+    background-color: #262626;
   }
 }
 
@@ -163,18 +215,38 @@ export default defineComponent({
 }
 
 table {
-  width: 100%;
-  table-layout: fixed;
+  display: flex;
+  flex-direction: column;
+}
+
+thead, tbody {
+  display: flex;
+  flex-direction: column;
+}
+
+tr {
+  display: flex;
 }
 
 th, td {
-  width: calc(100% / 6); /* I assumed 6 columns for this example. Adjust as needed. */
+  flex: 1;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 }
 
-html {
-  overflow-y: scroll; /* This will always show the vertical scrollbar */
+/* Specific adjustment for the Description column */
+th:nth-child(2), td:nth-child(2) {
+  flex: 3;
+}
+
+tr:hover td:first-child {
+  border-top-left-radius: 0.5rem; /* Increase for a more pronounced rounding */
+  border-bottom-left-radius: 0.5rem;
+}
+
+tr:hover td:last-child {
+  border-top-right-radius: 0.5rem;
+  border-bottom-right-radius: 0.5rem;
 }
 </style>
