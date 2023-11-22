74fbab841832e09e5860afce7b7b57039e922921
mq <iamborisbutenko@gmail.com>
2023-11-22 16:10:31 +0300

    .

--- src/entities/profile/Statistic.vue
+++ src/entities/profile/Statistic.vue
@@ -1,5 +1,5 @@
 <template>
 <ui-image
-    src="https://github.com/Game-Values/web-ui-widgets/blob/master/src/public/images/development-profile-statistic.png"
+    src="https://raw.githubusercontent.com/Game-Values/web-ui-widgets/master/src/public/images/development-profile-statistic.png"
 />
 </template>
