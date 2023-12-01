2f1c20c2a8077541839b4bb70f4e91325dee1149
mq <iamborisbutenko@gmail.com>
2023-12-01 21:14:04 +0300

    fixes api client

--- src/adapters/HttpAdapter.ts
+++ src/adapters/HttpAdapter.ts
@@ -1,12 +1,8 @@
-import type { FullRequestParams } from "#schema/http-client"
+import type { ApiConfig } from "#schema/http-client"
 // import type { QueryFunction, QueryKey, UseQueryReturnType } from "@tanstack/vue-query"
 import type { CookieClient } from "~/clients"
-import type { AsyncData } from "nuxt/dist/app"
-import type { FetchContext } from "ofetch"
 
 import { HttpClient } from "#schema/http-client"
-import { default as hash } from "hash-sum"
-import { serialize } from "object-to-formdata"
 
 import { HttpHeader, HttpToken } from "~/enums"
 
@@ -14,7 +10,24 @@ export class HttpAdapter extends HttpClient {
     public constructor(
         private _cookieClient: CookieClient,
     ) {
-        super()
+        let apiConfig: ApiConfig = {
+            baseApiParams: {
+                format: "json",
+            },
+            baseUrl: useRuntimeConfig().public.apiURL,
+        }
+
+        if (_cookieClient.accessToken) {
+            let tokenType: HttpToken = (
+                useUpperFirst(_cookieClient.tokenType || HttpToken.BEARER) as HttpToken
+            )
+
+            apiConfig.baseApiParams!.headers = {
+                [HttpHeader.AUTHORIZATION]: `${tokenType} ${_cookieClient.accessToken}`,
+            }
+        }
+
+        super(apiConfig)
     }
 
     // private async _queryFetch<T = any, E = any>(queryKey: QueryKey, queryFn: QueryFunction): UseQueryReturnType<T, E> {
@@ -34,48 +47,48 @@ export class HttpAdapter extends HttpClient {
     //     )
     // }
 
-    private get _requestParams(): Partial<FullRequestParams> & { baseURL: string } {
-        let requestParams: Partial<FullRequestParams> & { baseURL: string } = {
-            baseURL: useRuntimeConfig().public.apiURL,
-            credentials: "include",
-            headers: {
-                [HttpHeader.ACCEPT]: "application/json",
-            },
-        }
-
-        if (this._cookieClient.accessToken) {
-            let tokenType: HttpToken = (
-                useUpperFirst(this._cookieClient.tokenType || HttpToken.BEARER) as HttpToken
-            )
-
-            useSet(requestParams.headers!, HttpHeader.AUTHORIZATION, `${tokenType} ${this._cookieClient.accessToken}`)
-        }
-
-        return requestParams
-    }
-
-    // todo:
-    public async fetch<T, E>(requestParams: FullRequestParams): Promise<AsyncData<T, E>> {
-        let queryFn: () => Promise<AsyncData<T, E>> = (
-            (): Promise<AsyncData<T, E>> => useFetch<T, E>(requestParams.path, {
-                key: hash(requestParams),
-                onRequest: (context: FetchContext): void => {
-                    if (requestParams.body)
-                        if (isObject(requestParams.body))
-                            requestParams.body = serialize(
-                                cleanObject(requestParams.body),
-                            )
-                        else
-                            requestParams.body = JSON.stringify(requestParams.body)
-
-                    useMerge(context.options, this.mergeRequestParams(this._requestParams, requestParams))
-                },
-            })
-        )
-
-        // if (isClient() && isSetup())
-        //     return this._queryFetch([hash(requestParams)], queryFn)
-
-        return queryFn()
-    }
+    // private get _requestParams(): Partial<FullRequestParams> & { baseURL: string } {
+    //     let requestParams: Partial<FullRequestParams> & { baseURL: string } = {
+    //         baseURL: useRuntimeConfig().public.apiURL,
+    //         credentials: "include",
+    //         headers: {
+    //             [HttpHeader.ACCEPT]: "application/json",
+    //         },
+    //     }
+    //
+    //     if (this._cookieClient.accessToken) {
+    //         let tokenType: HttpToken = (
+    //             useUpperFirst(this._cookieClient.tokenType || HttpToken.BEARER) as HttpToken
+    //         )
+    //
+    //         useSet(requestParams.headers!, HttpHeader.AUTHORIZATION, `${tokenType} ${this._cookieClient.accessToken}`)
+    //     }
+    //
+    //     return requestParams
+    // }
+    //
+    // // todo:
+    // public async fetch<T, E>(requestParams: FullRequestParams): Promise<AsyncData<T, E>> {
+    //     let queryFn: () => Promise<AsyncData<T, E>> = (
+    //         (): Promise<AsyncData<T, E>> => useFetch<T, E>(requestParams.path, {
+    //             key: hash(requestParams),
+    //             onRequest: (context: FetchContext): void => {
+    //                 if (requestParams.body)
+    //                     if (isObject(requestParams.body))
+    //                         requestParams.body = serialize(
+    //                             cleanObject(requestParams.body),
+    //                         )
+    //                     else
+    //                         requestParams.body = JSON.stringify(requestParams.body)
+    //
+    //                 useMerge(context.options, this.mergeRequestParams(this._requestParams, requestParams))
+    //             },
+    //         })
+    //     )
+    //
+    //     // if (isClient() && isSetup())
+    //     //     return this._queryFetch([hash(requestParams)], queryFn)
+    //
+    //     return queryFn()
+    // }
 }
--- src/clients/HttpClient.ts
+++ src/clients/HttpClient.ts
@@ -2,17 +2,24 @@ import type { FullRequestParams } from "#schema/http-client"
 import type { HttpAdapter } from "~/adapters"
 import type { AsyncData } from "nuxt/dist/app"
 
+import { default as hash } from "hash-sum"
+
 export class HttpClient {
     public constructor(
         private _httpAdapter: HttpAdapter,
     ) {}
 
-    public async request<T, E>(requestParams: FullRequestParams): Promise<E | T> {
-        let asyncData: AsyncData<T, E> = await this._httpAdapter.fetch<T, E>(requestParams)
+    public async request<T, E>(requestParams: FullRequestParams): Promise<{
+        data: AsyncData<T, E>,
+        error: AsyncData<T, E>,
+    }> {
+        let { data, error }: AsyncData<T, E> = await useAsyncData<T, E>(hash(requestParams), (
+            (): Promise<T> => this._httpAdapter.request<T, E>(requestParams)
+        ))
 
-        return useMerge(asyncData, {
-            data: getRef(asyncData.data),
-            error: getRef(asyncData.error),
-        })
+        return {
+            data: getRef(data),
+            error: getRef(error),
+        }
     }
 }
