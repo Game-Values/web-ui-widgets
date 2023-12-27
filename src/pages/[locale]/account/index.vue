5fee173d9008d8352363c4dd9a2bc40ced28dda9
mq <iamborisbutenko@gmail.com>
2023-12-28 02:59:44 +0300

    account layout

--- src/pages/[locale]/account/index.vue
+++ src/pages/[locale]/account/index.vue
@@ -5,8 +5,10 @@ definePageMeta({
     middleware: RouteMiddleware.AUTH,
 })
 
-let { routerClient } = useClients()
+let { routerClient, storeClient } = useClients()
 let { userMeFacade } = useFacades()
+
+let { user } = storeToRefs(storeClient.userMeStore)
 
 await userMeFacade.bootstrap()
 </script>
@@ -14,27 +16,39 @@ await userMeFacade.bootstrap()
 <template>
 <ui-page>
     <v-row>
-        <v-column>
-            <v-title :level="1">
-                Settings
-            </v-title>
+        <v-column
+            :lg="18"
+            :md="16"
+        >
+            <v-row>
+                <v-column>
+                    <user-card :user="user" />
+                </v-column>
+
+                <v-column>
+                    <app-router-tabs
+                        :routes="[
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_BALANCE),
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_SALES),
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_PURCHASES),
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_ITEMS),
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_REVIEWS),
+                            routerClient.getRoute(routerClient.routeNames.ACCOUNT_SUBSCRIPTION),
+                        ]"
+                    />
+                </v-column>
+
+                <v-column>
+                    <nuxt-page />
+                </v-column>
+            </v-row>
         </v-column>
 
-        <v-column>
-            <app-router-tabs
-                :routes="[
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_BALANCE),
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_SALES),
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_PURCHASES),
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_ITEMS),
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_REVIEWS),
-                    routerClient.getRoute(routerClient.routeNames.ACCOUNT_SUBSCRIPTION),
-                ]"
-            />
-        </v-column>
-
-        <v-column>
-            <nuxt-page />
+        <v-column
+            :lg="6"
+            :md="8"
+        >
+            <entity-account-statistic />
         </v-column>
     </v-row>
 </ui-page>
