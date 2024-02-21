c40dbffde470412af4814b32ffa05ba22977c928
mq <iamborisbutenko@gmail.com>
2024-02-21 16:24:01 +0300

    feat: update script

--- scripts/api.ts
+++ scripts/api.ts
@@ -1,4 +1,4 @@
-import type { GenerateApiParamsFromSpecLiteral } from "swagger-typescript-api"
+import type { GenerateApiOutput, GenerateApiParamsFromSpecLiteral } from "swagger-typescript-api"
 import type { Spec } from "swagger-schema-official"
 
 import { resolve } from "node:path"
@@ -6,28 +6,33 @@ import { env } from "node:process"
 
 import { generateApi } from "swagger-typescript-api"
 
-await generateApi({
-    addReadonly: true,
-    enumNamesAsValues: true,
-    extractEnums: true,
-    extractRequestBody: true,
-    extractRequestParams: true,
-    extractResponseBody: true,
-    extractResponseError: true,
-    generateClient: true,
-    generateResponses: true,
-    generateRouteTypes: true,
-    generateUnionEnums: true,
-    name: "api.ts",
-    output: resolve("src", "shared", "schema"),
-    patch: true,
-    singleHttpClient: true,
-    sortRoutes: true,
-    sortTypes: true,
-    typePrefix: "I",
-    unwrapResponseData: true,
-    spec: (
-        await fetch(env.VITE_OPENAPI_URL)
-            .then((res: Response): Promise<Spec> => res.json() as Promise<Spec>)
-    ) satisfies Spec,
-} satisfies GenerateApiParamsFromSpecLiteral)
+function api(): Promise<GenerateApiOutput> {
+    return fetch(env.VITE_OPENAPI_URL!)
+        .then((res: Response): Promise<Spec> => res.json() as Promise<Spec>)
+        .then((spec: Spec): Promise<GenerateApiOutput> => (
+            generateApi({
+                addReadonly: true,
+                enumNamesAsValues: true,
+                extractEnums: true,
+                extractRequestBody: true,
+                extractRequestParams: true,
+                extractResponseBody: true,
+                extractResponseError: true,
+                generateClient: true,
+                generateResponses: true,
+                generateRouteTypes: true,
+                generateUnionEnums: true,
+                name: "api.ts",
+                output: resolve("src", "shared", "schema"),
+                patch: true,
+                singleHttpClient: true,
+                sortRoutes: true,
+                sortTypes: true,
+                typePrefix: "I",
+                unwrapResponseData: true,
+                spec,
+            } satisfies GenerateApiParamsFromSpecLiteral)
+        ))
+}
+
+await api()
